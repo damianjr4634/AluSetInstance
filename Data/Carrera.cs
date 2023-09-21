e41e2d2246942ffe0a1d16f498f8dd031a3df972
Damian <rdj4634@gmail.com>
2023-09-20 21:43:36 -0300

    cambios

---
+++ Data/Carrera.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AluSetInstance.Data
+{
+    public class Carrera
+    {
+        [Key]
+        public string Id { get; set; } = default!;
+        public string Nombre { get; set; } = default!;
+    }
+}
+
+/*
+create view carreras_web ( id, nombre )
+as
+select carre, descarre from carrera
+*/
